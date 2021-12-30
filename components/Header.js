7f518a8fed1f7db4f9df7a99d8ea4699ae3c3cdc
khr-rakib <khr.rakib@gmail.com>
2021-12-30 09:19:59 +0600

    menu certer - header text color change

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ export default function Header () {
     return (
         <>
             <header className="bg-gray-50 p-4 mb">
-                <div className="flex justify-between gap-4">
+                <div className="flex justify-between gap-4 items-center">
                     <div className="flex items-center space-x-5">
                         <div className="cursor-pointer sm:hidden " onClick={() => dispatch(sideNavAction(true))}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -16,11 +16,11 @@ export default function Header () {
                             </svg>   
                         </div>
                         <div>
-                            <div class="relative">
+                            <div className="relative">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 absolute text-gray-400 top-3 left-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
-                                <input type="text" class="bg-white h-10 w-full pl-12 pr-6 rounded-lg focus:outline-none hover:cursor-pointer" placeholder='Search'/>
+                                <input type="text" className="bg-white h-10 w-full pl-12 pr-6 rounded-lg focus:outline-none hover:cursor-pointer" placeholder='Search'/>
                             </div>
                         </div>
                     </div>
